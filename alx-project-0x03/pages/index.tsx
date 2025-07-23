ec57a44a25a0e89204ddb9687778f652e566f76e
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-23 15:01:16 +0100

    Update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,23 +1,11 @@
-const Home = () => {
-  return (
-    <div>
-      <h1> Welcome to Splash App </h1>
-    </div>
-  );
-};
-
-export default Home;
 import { PageRouteProps } from "@/interface";
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
 
-interface PageRouteProps {
-  pageRoute: string;
-}
-
 export default function Home() {
   const router = useRouter();
 
+  // Imperative routing with useRouter
   const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
     router.push(pageRoute, undefined, { shallow: false });
   };
